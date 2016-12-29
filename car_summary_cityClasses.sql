955465459fd8b6aa7cb942ee25ec9323436575ac
Leandro M Biondo <leandro.biondo@florestal.gov.br>
2016-12-29 11:54:20 -0200

    Create car_summary_cityClasses.sql

---
+++ car_summary_cityClasses.sql
@@ -0,0 +1,32 @@
+SELECT
+    usr_geocar_aplicacao.municipio.nom_municipio,
+    usr_geocar_aplicacao.tema.nom_tema,
+    SUM(usr_geocar_aplicacao.imovel.num_area_imovel),
+    SUM(usr_geocar_aplicacao.rel_tema_imovel_poligono.num_area)
+FROM
+    usr_geocar_aplicacao.rel_tema_imovel_poligono
+INNER JOIN
+    usr_geocar_aplicacao.tema
+ON
+    (
+        usr_geocar_aplicacao.rel_tema_imovel_poligono.idt_tema = usr_geocar_aplicacao.tema.idt_tema
+    )
+INNER JOIN
+    usr_geocar_aplicacao.imovel
+ON
+    (
+        usr_geocar_aplicacao.rel_tema_imovel_poligono.idt_imovel =
+        usr_geocar_aplicacao.imovel.idt_imovel)
+INNER JOIN
+    usr_geocar_aplicacao.municipio
+ON
+    (
+        usr_geocar_aplicacao.imovel.idt_municipio = usr_geocar_aplicacao.municipio.idt_municipio)
+WHERE
+    usr_geocar_aplicacao.imovel.flg_ativo = TRUE
+GROUP BY
+    usr_geocar_aplicacao.municipio.nom_municipio,
+    usr_geocar_aplicacao.tema.nom_tema
+ORDER BY
+    usr_geocar_aplicacao.imovel.nom_imovel ASC,
+    usr_geocar_aplicacao.tema.nom_tema ASC ;
