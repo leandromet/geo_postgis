4d5aa4e52d6f501760102ce4640a49a6a12b45aa
Leandro M Biondo <leandro.biondo@florestal.gov.br>
2016-12-29 11:47:51 -0200

    Create car_updateAlbersArea.sql

---
+++ car_updateAlbersArea.sql
@@ -0,0 +1,5 @@
+﻿--alter table sfb_result.sicar_int_rl_vn add column area_ha double precision;
+update sfb_result.sicar_int_rl_vn_201604_limpo set area_ha=st_area(st_transform(wkb_geometry, 102033))/10000;
+--select sum(area_ha) from sfb_result.sicar_int_rl_vn;
+--alter table sfb_result.sicar_int_app_vn add column area_ha double precision;
+--update sfb_result.sicar_int_app_vn_201604_limpo set area_ha=st_area(st_transform(wkb_geometry, 102033))/10000;
