fe2ca9573a35d2a6e3d75da431e87dd1210a4697
Leandro M Biondo <leandro.biondo@florestal.gov.br>
2016-12-29 11:47:02 -0200

    Create car_summary_smallProp.sql

---
+++ car_summary_smallProp.sql
@@ -0,0 +1,16 @@
+﻿SELECT 
+  municipio.nom_municipio, 
+  municipio.cod_estado, 
+  municipio.num_hectares_modulo_fiscal, 
+  imovel.ind_tipo_imovel,  
+  count(imovel.idt_imovel),
+  sum(num_area_imovel)
+
+FROM 
+  usr_geocar_aplicacao.municipio, 
+  usr_geocar_aplicacao.imovel
+WHERE 
+  municipio.idt_municipio = imovel.idt_municipio AND
+    imovel.flg_ativo = TRUE AND
+    (imovel.ind_status_imovel = 'AT' OR imovel.ind_status_imovel = 'PE')
+    AND imovel.num_modulo_fiscal<4;
