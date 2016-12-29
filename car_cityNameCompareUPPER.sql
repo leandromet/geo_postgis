72d7f73cd7211f1ba75e275fc20f2f5f22e1787b
Leandro M Biondo <leandro.biondo@florestal.gov.br>
2016-12-29 11:45:20 -0200

    Create car_cityNameCompareUPPER.sql

---
+++ car_cityNameCompareUPPER.sql
@@ -0,0 +1,14 @@
+﻿SELECT 
+  municipio.idt_municipio, 
+  municipios_mda_uf.idt_municipio2, 
+  municipios_mda_uf.cod_estado2, 
+  municipio.cod_estado, 
+  municipio.nom_municipio, 
+  municipios_mda_uf.nom_municipio as muni_mda
+FROM 
+  usr_geocar_aplicacao.municipio, 
+  sfb_result.municipios_mda_uf
+WHERE 
+municipios_mda_uf.cod_estado2=municipio.cod_estado AND
+  REPLACE(usr_geocar_aplicacao.unaccent_string(UPPER(municipio.nom_municipio)), ' ','') LIKE REPLACE(usr_geocar_aplicacao.unaccent_string(municipios_mda_uf.nom_municipio), ' ','')
+  order by cod_estado, muni_mda;
