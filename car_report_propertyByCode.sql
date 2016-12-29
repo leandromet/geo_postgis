949da71b6eee167ff2155c7832659f3adfd939cc
Leandro M Biondo <leandro.biondo@florestal.gov.br>
2016-12-29 15:55:14 -0200

    Create car_report_propertyByCode.sql

---
+++ car_report_propertyByCode.sql
@@ -0,0 +1,12 @@
+﻿SELECT 
+  imovel.idt_imovel, 
+  imovel.cod_imovel, 
+  imovel_pessoa.idt_imovel_pessoa, 
+  imovel_pessoa.cod_cpf_cnpj, 
+  imovel.num_area_imovel
+FROM 
+  usr_geocar_aplicacao.imovel_pessoa, 
+  usr_geocar_aplicacao.imovel
+WHERE 
+  imovel.cod_imovel in ('codigo','codigo','codigo')
+  and imovel_pessoa.idt_imovel = imovel.idt_imovel;
