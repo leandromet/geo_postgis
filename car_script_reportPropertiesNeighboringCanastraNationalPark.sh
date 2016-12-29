b50bf05bb545004f18c7e057ef8a5ce7f93f10b7
Leandro M Biondo <leandro.biondo@florestal.gov.br>
2016-12-29 16:25:55 -0200

    Update car_script_reportPropertiesNeighboringCanastraNationalPark.sh

--- car_script_reportPropertiesNeighboringCanastraNationalPark.sh
+++ car_script_reportPropertiesNeighboringCanastraNationalPark.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+##Geometry
 
-
+		ogr2ogr -f "ESRI Shapefile"   parna_canastra_saojoaquim.shp PG:"host=host.144 user=user dbname=db"  -sql "SELECT    imovel.idt_imovel,  imovel.ind_tipo_imovel, imovel.nom_imovel,    imovel.num_area_imovel as area_imovel, imovel.num_modulo_fiscal, imovel.geo_area_imovel as geom FROM   usr_geocar_aplicacao.imovel  where  idt_municipio in (3112802,3121209,3156908,3162203,3164308,3170602,4202503,4206108,4209607,4211702,4218905) and ind_status_imovel in ('AT','PE')" 
+##People
 		ogr2ogr -f "CSV"   parna_canastra_saojoaquim_pessoas.csv PG:"host=host.144 user=user dbname=db"  -sql "SELECT    imovel.idt_imovel,    imovel_pessoa.ind_tipo_pessoa,    imovel_pessoa.nom_completo,    idt_documento_imovel,   documento_imovel.ind_tipo_documento,    documento.nom_documento FROM    usr_geocar_aplicacao.imovel,    usr_geocar_aplicacao.imovel_pessoa,    usr_geocar_aplicacao.documento,    usr_geocar_aplicacao.documento_imovel WHERE    idt_municipio in (3112802,3121209,3156908,3162203,3164308,3170602,4202503,4206108,4209607,4211702,4218905) and ind_status_imovel in ('AT','PE') and   imovel_pessoa.idt_imovel = imovel.idt_imovel AND   documento.idt_documento = documento_imovel.ind_tipo_documento AND   documento_imovel.idt_imovel = imovel.idt_imovel" 
