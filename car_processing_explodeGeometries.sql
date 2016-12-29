3acb0111b550a48a1cec3f7cbe0439bf1f5bee37
Leandro M Biondo <leandro.biondo@florestal.gov.br>
2016-12-29 11:57:10 -0200

    Create car_processing_explodeGeometries.sql

---
+++ car_processing_explodeGeometries.sql
@@ -0,0 +1,7 @@
+﻿drop table sfb_result.sicar_int_rl_vn_201604_limpo;
+create table sfb_result.sicar_int_rl_vn_201604_limpo as
+select id, geom, idt_municipio from (select ogc_fid as id, idt_municipio, ST_GeometryN(wkb_geometry,generate_series(1,ST_NumGeometries(wkb_geometry))) AS geom from sfb_result.sicar_int_rl_vn_201604)
+ as foo where geometrytype(geom)='POLYGON';
+
+
+--select distinct GeometryType(geom) from sfb_result.sicar_int_rl_vn_201604_limpo;
