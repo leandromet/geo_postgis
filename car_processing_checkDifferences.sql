4001b84e4eb49a73f4faca06878aaede9211c1b2
Leandro M Biondo <leandro.biondo@florestal.gov.br>
2016-12-29 11:58:29 -0200

    Create car_processing_checkDifferences.sql

---
+++ car_processing_checkDifferences.sql
@@ -0,0 +1,7 @@
+﻿create table sfb_result.sicar_int_munic_falta as
+SELECT 
+  municipio.idt_municipio
+FROM 
+  sfb_apoio.municipio full outer join sfb_result.sicar_int_munic_old using (idt_municipio)
+WHERE 
+ sfb_result.sicar_int_munic_old.idt_municipio is null;
