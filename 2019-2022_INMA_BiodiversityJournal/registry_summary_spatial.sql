81b536add0fc50d735aedd469e549d0855cc40da
Leandro M Biondo <leandro.biondo@florestal.gov.br>
2022-09-20 11:00:38 -0300

    Update registry_summary_spatial.sql

--- 2019-2022_INMA_BiodiversityJournal/registry_summary_spatial.sql
+++ 2019-2022_INMA_BiodiversityJournal/registry_summary_spatial.sql
@@ -1,40 +1,129 @@
+sibbr
 
-select nm_municip, selc1."class", sum(imv) as imv, sum(app) as app,  sum(rvn) as rvn, sum(rl) as rl from
+select nm_municip, selc1.kingdom, selc1."class", selc1."order", selc1.family, sum(sibbr) as sibbr, sum(imv) as imv, sum(app) as app,  sum(rvn) as rvn, sum(rl) as rl from
+
+dados_externos.ibge_br_mu_250gc_2018
+, (select nm_municip as nm_munic_sibbr,kingdom, "class", "order",family , count ( distinct sibbr.id) as sibbr	   
+from
+inma_processamento.es_serra_colec_sibbr sibbr, dados_externos.ibge_br_mu_250gc_2018 munic
+where nm_municip in ( 'SANTA TERESA',
+'SANTA MARIA DE JETIBÁ',
+'SANTA LEOPOLDINA',
+'ITAGUAÇU',
+'ITARANA') and
+st_INTERSECTS(sibbr.geom, munic.geom)
+group by nm_municip,kingdom, "class", "order" , family) selc0
+
+, (select municipio_,kingdom, "class", "order", family, count ( distinct sibbr.id) as app	   
+from
+inma_processamento.boletim_espacial_car_app app
+left outer join
+inma_processamento.es_serra_colec_sibbr sibbr
+on
+st_intersects(sibbr.geom, app.geom)
+group by municipio_,kingdom, "class", "order", family ) selc1
+
+, (select municipio_,kingdom, "class", "order", family, count ( distinct sibbr.id) as imv 	   
+from
+inma_processamento.boletim_espacial_car_imovel imv
+left outer join
+inma_processamento.es_serra_colec_sibbr sibbr
+on
+st_intersects(sibbr.geom, imv.geom)
+group by municipio_,kingdom, "class", "order", family ) selc2
+
+,(select municipio_,kingdom, "class", "order", family, count ( distinct sibbr.id) as rvn 	   
+from
+inma_processamento.boletim_espacial_car_remanescente rvn
+left outer join
+inma_processamento.es_serra_colec_sibbr sibbr
+on
+st_intersects(sibbr.geom, rvn.geom)
+group by municipio_,kingdom, "class", "order", family ) selc3
+
+,(select municipio_,kingdom, "class", "order", family, count ( distinct sibbr.id) as rl 	   
+from
+inma_processamento.boletim_espacial_car_reserva_legal rl
+left outer join
+inma_processamento.es_serra_colec_sibbr sibbr
+on
+st_intersects(sibbr.geom, rl.geom)
+group by municipio_,kingdom, "class", "order", family ) selc4
+
+
+where 
+selc1.municipio_ = cast(ibge_br_mu_250gc_2018.cd_geocmu as int)
+and nm_municip = nm_munic_sibbr
+and selc1.municipio_ = selc2.municipio_ 
+and selc1.municipio_ = selc3.municipio_ 
+and selc1.municipio_ = selc4.municipio_
+and selc1."class" = selc2."class"
+and selc1."class" = selc3."class"
+and selc1."class" = selc4."class"
+and selc1."class" = selc0."class"
+and selc1."order" = selc2."order"
+and selc1."order" = selc3."order"
+and selc1."order" = selc4."order"
+and selc1."order" = selc0."order"
+and selc1.family = selc2.family
+and selc1.family = selc3.family
+and selc1.family = selc4.family
+and selc1.family = selc0.family
+
+group by nm_municip, selc1.kingdom, selc1."class", selc1."order",selc1.family
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gbif
+
+select nm_municip, selc1."class",selc1."order", selc1.family, sum(imv) as imv, sum(app) as app,  sum(rvn) as rvn, sum(rl) as rl from
 
 
 dados_externos.ibge_br_mu_250gc_2018,
-(select nm_municip as nm_munic_gbif,"class", count ( distinct gbif.id) as gbif	   
+(select nm_municip as nm_munic_gbif,kingdom,"class","order", family, count ( distinct gbif.id) as gbif	   
 from
 inma_processamento.es_serra_colec_gbif gbif
-group by nm_municip,"class" ) selc0
+group by nm_municip,kingdom,"class","order", family ) selc0
 ,
-(select municipio_,"class", count ( distinct gbif.id) as app	   
+(select municipio_,kingdom,"class","order", family, count ( distinct gbif.id) as app	   
 from
 inma_processamento.boletim_espacial_car_app app
 left outer join
 inma_processamento.es_serra_colec_gbif gbif
 on
 st_intersects(gbif.geom, app.geom)
-group by municipio_,"class" ) selc1
-, (select municipio_,"class", count ( distinct gbif.id) as imv 	   
+group by municipio_,kingdom,"class","order", family ) selc1
+, (select municipio_,kingdom,"class","order", family, count ( distinct gbif.id) as imv 	   
 from
 inma_processamento.boletim_espacial_car_imovel imv
 left outer join
 inma_processamento.es_serra_colec_gbif gbif
 on
 st_intersects(gbif.geom, imv.geom)
-group by municipio_, "class" ) selc2
+group by municipio_,kingdom,"class","order", family ) selc2
 
-, (select municipio_,"class", count ( distinct gbif.id) as rvn 	   
+, (select municipio_,kingdom,"class","order", family, count ( distinct gbif.id) as rvn 	   
 from
 inma_processamento.boletim_espacial_car_remanescente rvn
 left outer join
 inma_processamento.es_serra_colec_gbif gbif
 on
 st_intersects(gbif.geom, rvn.geom)
-group by municipio_,"class" ) selc3
+group by municipio_,kingdom,"class","order", family ) selc3
 
-, (select municipio_,"class", count ( distinct gbif.id) as rl 
+, (select municipio_,kingdom,"class","order", family, count ( distinct gbif.id) as rl 
 		   
 from
 inma_processamento.boletim_espacial_car_reserva_legal rl
@@ -43,7 +132,7 @@ inma_processamento.es_serra_colec_gbif gbif
 on
 st_intersects(gbif.geom, rl.geom)
 
-group by municipio_,"class" ) selc4
+group by municipio_,kingdom,"class","order", family ) selc4
 
 
 where 
@@ -56,8 +145,16 @@ and selc1."class" = selc2."class"
 and selc1."class" = selc3."class"
 and selc1."class" = selc4."class"
 and selc1."class" = selc0."class"
-group by nm_municip, selc1."class"
+and selc1."order" = selc2."order"
+and selc1."order" = selc3."order"
+and selc1."order" = selc4."order"
+and selc1."order" = selc0."order"
+and selc1.family = selc2.family
+and selc1.family = selc3.family
+and selc1.family = selc4.family
+and selc1.family = selc0.family
 
+group by nm_municip, selc1.kingdom, selc1."class", selc1."order",selc1.family
 
 
 
